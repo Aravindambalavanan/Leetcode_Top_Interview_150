eb60132d8a6edc8d2ba351a8560c4c5844871e23
Aravindambalavanan <j_aravind@outlook.com>
2023-08-31 10:07:37 +0530

    Added Java Solutions

---
+++ Stack/Min Stack/MinStack.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+
+class MinStack {
+    Stack<PairStack> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            stack.push(new PairStack(val, val));
+        } else {
+            int currentMinInStack = stack.peek().min;
+            stack.push(new PairStack(val, Math.min(currentMinInStack, val)));
+        }
+    }
+
+    public void pop() {
+        if (!stack.isEmpty()) {
+            stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek().val;
+    }
+
+    public int getMin() {
+        return stack.peek().min;
+    }
+
+    class PairStack {
+        int val;
+        int min;
+
+        PairStack(int val, int min) {
+            this.val = val;
+            this.min = min;
+        }
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
---
+++ Stack/Valid Parentheses/ValidParentheses.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (char x : s.toCharArray()) {
+            if (x == '(') {
+                stack.push(')');
+            } else if (x == '{') {
+                stack.push('}');
+            } else if (x == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != x) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
