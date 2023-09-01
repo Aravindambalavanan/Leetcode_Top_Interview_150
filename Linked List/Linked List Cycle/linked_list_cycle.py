528aa15e4f1caf4870f093f71e0db557561b8a0f
Akhil Robert <akhilrobert2412@gmail.com>
2023-09-01 17:11:50 +0530

    removed comments

--- Linked List/Linked List Cycle/linked_list_cycle.py
+++ Linked List/Linked List Cycle/linked_list_cycle.py
@@ -1,10 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
--- Linked List/Merge Two Sorted Lists/merge_sorted_lists.py
+++ Linked List/Merge Two Sorted Lists/merge_sorted_lists.py
@@ -1,8 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution:
     def mergeTwoLists(
         self, list1: Optional[ListNode], list2: Optional[ListNode]
